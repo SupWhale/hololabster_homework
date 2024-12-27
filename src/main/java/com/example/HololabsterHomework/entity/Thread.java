5946f35153421383ea57159d7eb5b21d7b275c28
권재현 <kmjbs313@gmail.com>
2024-12-27 15:59:01 +0900

    Delete src directory

--- src/main/java/com/example/HololabsterHomework/entity/Thread.java
+++
@@ -1,8 +0,0 @@
-package com.example.HololabsterHomework.entity;
-
-public class Thread {
-    public int thread_id;
-    public int result;
-
-
-}
--- src/main/java/com/example/schedulemanage/entity/Thread.java
+++
@@ -1,8 +0,0 @@
-package com.example.schedulemanage.entity;
-
-public class Thread {
-    public int thread_id;
-    public int result;
-
-
-}
