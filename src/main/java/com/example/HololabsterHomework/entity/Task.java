d6edad57c0b72e84908f2327b294c7f2a3a3e42b
권재현 <kmjbs313@gmail.com>
2024-12-27 15:59:30 +0900

    소스 정리 및 어플리케이션 이름 변경
    소스 정리 및 어플리케이션 이름 변경

---
+++ src/main/java/com/example/HololabsterHomework/entity/Task.java
@@ -0,0 +1,11 @@
+package com.example.HololabsterHomework.entity;
+
+import java.util.List;
+
+public class Task {
+    public String status;
+    public List<ProcessInfo> processInfo;
+    public int totalSum;
+    public int process_count=0;
+    public int threads_per_process=0;
+}
