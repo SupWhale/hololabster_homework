174698e13fb1f8b7dc80a9de359f450f6699025f
권재현 <kmjbs313@gmail.com>
2024-12-27 12:34:16 +0900

    Update Packge Name

---
+++ src/main/java/com/example/HololabsterHomework/entity/ProcessInfo.java
@@ -0,0 +1,10 @@
+package com.example.HololabsterHomework.entity;
+
+import java.util.List;
+
+public class ProcessInfo {
+    public String process_id;
+    public static int process_sum = 0;
+    public List<Thread> threads;
+
+}
---
+++ src/main/java/com/example/HololabsterHomework/entity/Thread.java
@@ -0,0 +1,8 @@
+package com.example.HololabsterHomework.entity;
+
+public class Thread {
+    public int thread_id;
+    public int result;
+
+
+}
